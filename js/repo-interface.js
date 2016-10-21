6a2b17e1f90e4689f5589b2feb31a0980dc2128c
Anand Angalig <anandangalig@gmail.com>
2016-10-21 13:53:35 -0400

    add repo display section with links for each

--- js/repo-interface.js
+++ js/repo-interface.js
@@ -1,10 +1,8 @@
 var Repository = require('./../js/repo.js').repoModule;
 
 var displayRepos = function(repo) {
-  console.log(repo[0].name);
   for (i = 0; i < repo.length; i++) {
-    $('#showRepos').append("<li>" + repo[i].name + "</li>");
-    console.log(repo[i].name);
+    $('#showRepos').append("<li>" + "<a href='" + repo[i].clone_url + "'" + "target='_blank'>" + repo[i].name + "</a>" + "</li>");
   }
 };
 
@@ -13,8 +11,8 @@ $(document).ready(function() {
   var newUserRepository = new Repository();
   $('#newNameInput').submit(function(event) {
     event.preventDefault();
-    var username = $('#username').val();
-    $('#username').val("");
     var repositories = newUserRepository.getRepos(displayRepos);
+    $('#username').val("");
+    $("#showRepos").html("");
   });
 });
--- js/repo.js
+++ js/repo.js
@@ -1,10 +1,10 @@
 var apiKey = require('./../.env').apiKey;
-
 Repository = function() {
 };
 
 Repository.prototype.getRepos = function(displayFunction){
-  $.get('https://api.github.com/users/anandangalig/repos?access_token=' + apiKey).then(function(response){
+  var username = $('#username').val();
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
     displayFunction(response);
   }).fail(function(error){
     $('showRepos').text(error.responseJSON.message);
